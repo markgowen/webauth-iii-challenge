7954c2b324e40c84457705a2bba4af6978a70b97
Mark Gowen <mark.gowen78@gmail.com>
2019-11-13 11:41:15 -0800

    Project Complete

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const config = require("./middleware/config-mw");
 
 const authRouter = require("./auth/auth-router");
+const userRouter = require("./routes/user-router");
 
 const server = express();
 
 config(server);
 
 server.use("/api/auth", authRouter);
+server.use("/api/users", userRouter);
 
 module.exports = server;
